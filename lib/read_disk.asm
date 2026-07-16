cdddc1328e871e0aefba9e5fe6c51381305e2001
mehmedaltug <mehmed.ede@hotmail.com>
2026-07-16 17:20:52 +0300

    Moved reading to its own func.
    TODO: make this a function (non jumpable)

---
+++ lib/read_disk.asm
@@ -0,0 +1,30 @@
+BOOT_DRIVE db 0
+
+; Read al sectors, from drive 0 and map result to bx
+READ_DISK:
+    push bx
+    push ax
+    mov [BOOT_DRIVE], dl
+    mov al, "B"
+    mov ah, 0x0e
+    mov bh, 0
+    mov bl, 3
+    int 0x10
+    
+    pop ax
+    mov ah, 0x02                   ; disk read code
+    mov ch, 0                      ; cylinder
+    mov dh, 0                      ; disk number
+    mov cl, 2                      ; where to start reading (bootloader is 1)
+    mov dl, [BOOT_DRIVE]           ; drive number to read
+    mov bx, KERNEL                 ; where to put it
+    int 0x13
+    
+    jnc noError
+    mov al, "E"
+    mov ah, 0x0e
+    int 0x10
+    
+    noError:
+        pop bx
+        jmp bx
