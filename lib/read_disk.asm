b0b24de65e7dcf8064ae75d17d847ae8ed443579
mehmedaltug <mehmed.ede@hotmail.com>
2026-07-18 07:52:49 +0300

    refactor read_disk.asm to simplify disk read logic and remove unused variables

--- lib/read_disk.asm
+++ lib/read_disk.asm
@@ -1,25 +1,10 @@
-BOOT_DRIVE db 0
+; Read al sectors, starting from cl (1 is bootloader), from the BIOS drive in DL
 
-; Read al sectors, starting from cl (1 is bootloader), from drive 0 and map result to bx
 READ_DISK:
     pusha
-    push cx
-    push bx
-    push ax
-    mov [BOOT_DRIVE], dl
-    mov al, "B"
-    mov ah, 0x0e
-    mov bh, 0
-    mov bl, 3
-    int 0x10
-    
-    pop ax
-    pop bx
-    pop cx
     mov ah, 0x02                   ; disk read code
     mov ch, 0                      ; cylinder
-    mov dh, 0                      ; disk number
-    mov dl, [BOOT_DRIVE]           ; drive number to read
+    mov dh, 0                      ; head
     int 0x13
     
     jnc noError
