8d96f66159794964cc364bad260644af7b3f64b9
mehmedaltug <mehmed.ede@hotmail.com>
2026-09-09 18:19:19 +0300

    changed bx usage to si and used bh to specify page num

--- lib/print.asm
+++ lib/print.asm
@@ -1,14 +1,15 @@
-; takes bx as argument string
+; takes si as argument string
 
 print:
 	pusha
 	mov ah, 0x0e
+	mov bh, 0
 	print_loop:
-		mov al, [bx]
+		mov al, [si]
 		cmp al, 0
 		je print_end
 		int 0x10
-		inc bx
+		inc si
 		jmp print_loop
 	print_end:
 		popa
